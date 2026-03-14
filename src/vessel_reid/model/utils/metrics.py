ec9b809a463cb5192b61cde8c6a43e61402c6dd5
maksimovic-milica <milicamaksimoviic@gmail.com>
2026-03-14 15:51:39 -0500

    rank k accuracy function

--- src/vessel_reid/model/utils/metrics.py
+++ src/vessel_reid/model/utils/metrics.py
@@ -11,3 +11,28 @@ def rank1_accuracy(distances: np.ndarray, labels: np.ndarray, query_labels: np.n
     top1 = np.argmin(distances, axis=1)
     matches = labels[top1] == query_labels
     return float(matches.mean())
+
+
+def compute_rankk_accuracy(
+    similarity_matrix: np.ndarray,
+    query_ids: np.ndarray,
+    gallery_ids: np.ndarray,
+    k: int,
+) -> float:
+    """
+    Rank-k accuracy: fraction of queries where the correct ID shows up in top-k results.
+
+    Args:
+        similarity_matrix: shape (num_queries, num_gallery), higher = more similar
+        query_ids: shape (num_queries,)
+        gallery_ids: shape (num_gallery,)
+        k: number of top results to consider
+    """
+    num_queries = similarity_matrix.shape[0]
+    top_k_indices = np.argsort(similarity_matrix, axis=1)[:, ::-1][:, :k]
+    correct = 0
+    for i in range(num_queries):
+        top_k_ids = gallery_ids[top_k_indices[i]]
+        if query_ids[i] in top_k_ids:
+            correct += 1
+    return correct / num_queries
